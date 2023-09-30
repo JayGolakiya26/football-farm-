d9f4cf168053d0675a5721462ede8baa3a63a777
JayGolakiya26 <123795648+JayGolakiya26@users.noreply.github.com>
2023-09-30 14:10:25 +0530

    update : edit in type handle undefine

--- src/utils/auth.utils.ts
+++ src/utils/auth.utils.ts
@@ -1,11 +1,15 @@
+import { SignUpUserState } from "src/type/api/slice/auth"
 
-export const formatRegisterRequestParams = (values: any)=> {
-    return {
-      login_type: values.loginType,
-      age_terms_accepted:values.ageTermsAccepted,
-      first_name:values.firstName,
-      last_name:values.lastName,
-      email:values.email,
-      reference_number:values.referenceNumber
-    }
+export const formatRegisterRequestParams = (values?: SignUpUserState)=> {
+  if (!values) {
+    throw new Error("Values are undefined"); 
   }
+  return {
+    login_type: values.loginType,
+    age_terms_accepted: values.ageTermsAccepted,
+    first_name: values.firstName,
+    last_name: values.lastName,
+    email: values.email,
+    reference_number: values.referenceNumber
+  }
+}
